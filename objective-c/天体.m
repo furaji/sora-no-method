da739629f1d903f08b03d5b843c59320edc3f251
Yuki MIZUNO <mizuyuu0904@gmail.com>
2016-10-06 14:14:51 +0900

    Objective-Cの実装を追加

---
+++ objective-c/天体.m
@@ -0,0 +1,20 @@
+#import <Foundation/Foundation.h>
+
+@interface 天体 : NSObject
+- (void)メソッド;
+@end
+
+@implementation 天体
+- (void)メソッド {
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+    [[NSFileHandle fileHandleWithStandardOutput] writeData: [@"天体のメソッド" dataUsingEncoding: NSUTF8StringEncoding]];
+    [pool release];
+}
+@end
+
+int main(void) {
+    id _天体 = [[天体 alloc] init];
+    [_天体 メソッド];
+
+    return 0;
+}
